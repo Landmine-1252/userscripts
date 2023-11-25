77e80e1b28645c136e5cb79dd93c32b235cfcc06
Landmine <landmine@outlook.com>
2023-11-25 13:10:29 -0800

    Add `downloadURL` url

--- youtube-shorts-redirect.js
+++ youtube-shorts-redirect.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         YouTube Short Redirect
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  YouTube Short links are redirected to normal video links
 // @author       Landmine
 // @match        *://www.youtube.com/*
 // @match        *://youtube.com/*
+// @downloadURL  https://raw.githubusercontent.com/Landmine-1252/userscripts/main/youtube-shorts-redirect.js
+// @updateURL    https://raw.githubusercontent.com/Landmine-1252/userscripts/main/youtube-shorts-redirect.js
 // ==/UserScript==
 
 (function() {
